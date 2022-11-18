b5f97db8f64071fc6c6192ba8b2afc90b02c78b0
JAVERIA SHAIKH <108260808+javeriashaikh552@users.noreply.github.com>
2022-11-17 23:54:03 -0800

    ok go

--- login.php
+++ login.php
@@ -7,18 +7,104 @@
     <title>Document</title>
 </head>
 <body>
+    
+    <div class="container ">
+
     <form action="#" method="POST">
 
-<input type="email" place holder="enter your email" name="email">
+    
+<label >EMAIL</label>
+<input type="email" placeholder="enter your email" name="email">
+
+<br>
+<br>
+<label >PASSWORD</label>
+<input type="password" placeholder="enter your password" name="pass">
 
 
+<br>
+<br>
+<br>
+<br>
 
-<input type="password" place holder="enter your password" name="pass">
-
-
-
-<input type="submit" class="btn btn-success">
+<input type="submit" class="btn btn-primary">
 
     </form>
+  
+    </div>
 </body>
 </html>
+
+
+<?php
+
+// if(isset($_POST["submit"])){
+//     session_start();
+
+//     $conn = mysqli_connect("localhost", "root", "", "userforms"); 
+
+   
+// $email =$_POST['email'];
+// $pass =$_POST['pass'];
+
+// $query= "SELECT * FROM `user` where `email` ='$email' AND `pass`= '$pass'";
+
+// $resutl = mysqli_query ($conn,$query);
+
+// if(mysqli_num_rows($result))
+// {
+//     while($data =mysqli_fetch_assoc($result)){
+//         $_SESSION["name"]=$data["name"];
+//         // header("Location:index.php");
+//     }
+// }
+// else{
+//     echo "invalid email os password";
+// }
+
+// }
+
+?>
+<!-- <script>
+    window.location.assign("index.php");
+</script> -->
+
+
+<?php
+
+
+
+if (isset($_POST["submit"])) {
+session_start();    
+
+
+
+$conn = mysqli_connect("localhost","root","","userforms");
+
+
+
+$email = $_POST["email"];
+$pass = $_POST["pass"];
+
+$query =   "SELECT * FROM `user` WHERE `email` = '$email' AND `pass` = '$pass'";
+
+
+
+$result = mysqli_query($conn,$query);
+
+if(mysqli_num_rows($result))
+{
+    while($data  = mysqli_fetch_assoc($result)){
+            $_SESSION["name"]  = $data["name"];
+            header("Location: index.php");
+
+    }
+}else{
+    echo "invalid email or password";
+}   
+
+
+
+}
+
+?>
