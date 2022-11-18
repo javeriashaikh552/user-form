e57042bf548fad86b9820aab557786bdd4287232
JAVERIA SHAIKH <108260808+javeriashaikh552@users.noreply.github.com>
2022-11-17 22:44:14 -0800

    push

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="POST">
+
+<input type="email" place holder="enter your email" name="email">
+
+
+
+<input type="password" place holder="enter your password" name="pass">
+
+
+
+<input type="submit" class="btn btn-success">
+
+    </form>
+</body>
+</html>
